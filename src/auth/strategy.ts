b5fe70baa868d45f2c04d1e4e06d350a3f125159
jotarlo <Jeferson Arango Lpez>
2023-03-08 12:04:07 -0500

    se agrega la configuración y la estrategia inicial. Falta integrar con ms de seguridad

---
+++ src/auth/strategy.ts
@@ -0,0 +1,45 @@
+import {AuthenticationStrategy} from '@loopback/authentication';
+import {AuthenticationBindings} from '@loopback/authentication/dist/keys';
+import {AuthenticationMetadata} from '@loopback/authentication/dist/types';
+import {inject} from '@loopback/context';
+import {HttpErrors, Request} from '@loopback/rest';
+import {UserProfile} from '@loopback/security';
+import parseBearerToken from 'parse-bearer-token';
+
+export class AuthStrategy implements AuthenticationStrategy {
+  name: string = 'auth';
+
+  constructor(
+    @inject(AuthenticationBindings.METADATA)
+    private metadata: AuthenticationMetadata[],
+  ) {
+
+  }
+
+  /**
+   * Autenticación de un usuario frente a una acción en la base de datos
+   * @param request la solicitud con el token
+   * @returns el perfil de usuario, undefined cuando no tiene permiso o un httpError
+   */
+  async authenticate(request: Request): Promise<UserProfile | undefined> {
+    let token = parseBearerToken(request);
+    if (token) {
+      let idMenu: string = this.metadata[0].options![0];
+      let accion: string = this.metadata[0].options![1];
+      console.log(this.metadata);
+      // contectar con el ms de seguridad
+      console.log("Conectar con ms-seguridad")
+
+      let continuar: boolean = false;
+      if (continuar) {
+        let perfil: UserProfile = Object.assign({
+          permitido: "OK"
+        });
+        return perfil;
+      } else {
+        return undefined;
+      }
+    }
+    throw new HttpErrors[401]("No es posible ejecutar la acción por falta de un token.");
+  }
+}
---
+++ src/config/configuracion.seguridad.ts
@@ -0,0 +1,8 @@
+export namespace ConfiguracionSeguridad {
+  export const menuProductoId = "63d05003013402040c04096a";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
+}
